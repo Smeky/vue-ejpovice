863497852328d37fe4e2bcd09f740f70708e7a69
Tomas Sobota <tomas.sobota@mediaform.cz>
2022-12-05 15:22:19 +0100

    Added hover effect to grades rectangle

--- components/GradesRectangle.vue
+++ components/GradesRectangle.vue
@@ -1,36 +1,19 @@
 <template>
     <div class="grades-rectangle">
-        <div class="grades-rectangle--grade grades-rectangle--grade--topleft">
-            <img src="~/assets/icons/grade-health.svg" />
-            <span class="text-sm-title color-white">zdravé bydlení</span>
-            
-            <WithArrow class="grades-rectangle--grade--more mt-4">
-                <span class="text-mb-button color-white uppercase">více</span>
-            </WithArrow>
-        </div>
-        <div class="grades-rectangle--grade grades-rectangle--grade--topright">
-            <img src="~/assets/icons/grade-comfort.svg" />
-            <span class="text-sm-title color-white">vysoký komfort</span>
-            
-            <WithArrow class="grades-rectangle--grade--more mt-4">
-                <span class="text-mb-button color-white uppercase">více</span>
-            </WithArrow>
-        </div>
-        <div class="grades-rectangle--grade grades-rectangle--grade--bottomleft">
-            <img src="~/assets/icons/grade-savings.svg" />
-            <span class="text-sm-title color-white">úspora nákladů</span>
-            
-            <WithArrow class="grades-rectangle--grade--more mt-4">
-                <span class="text-mb-button color-white uppercase">více</span>
-            </WithArrow>
-        </div>
-        <div class="grades-rectangle--grade grades-rectangle--grade--bottomright">
-            <img src="~/assets/icons/grade-gentle.svg" />
-            <span class="text-sm-title color-white">šetrnost stavby</span>
-            
-            <WithArrow class="grades-rectangle--grade--more mt-4">
-                <span class="text-mb-button color-white uppercase">více</span>
-            </WithArrow>
+        <div v-for="(grade, index) in grades" :key="`grades-${index}`"
+             class="grades-rectangle--grade-container"
+             :class="grade.class"
+        >
+            <div class="grades-rectangle--grade py-12">
+                <img :src="grade.icon" />
+                <span class="text-sm-title color-white">
+                    {{ grade.title }}
+                </span>
+                
+                <WithArrow class="grades-rectangle--grade--more mt-4">
+                    <span class="text-md-button color-white uppercase">více</span>
+                </WithArrow>
+            </div>
         </div>
     </div>
 </template>
@@ -39,7 +22,30 @@
 
 <script>
 export default {
-
+    data: () => ({
+        grades: [
+            {
+                icon: require('~/assets/icons/grade-health.svg'),
+                title: 'zdravé bydlení',
+                class: 'grades-rectangle--grade--topleft'
+            },
+            {
+                icon: require('~/assets/icons/grade-comfort.svg'),
+                title: 'vysoký komfort',
+                class: 'grades-rectangle--grade--topright'
+            },
+            {
+                icon: require('~/assets/icons/grade-savings.svg'),
+                title: 'úspora nákladů',
+                class: 'grades-rectangle--grade--bottomleft'
+            },
+            {
+                icon: require('~/assets/icons/grade-gentle.svg'),
+                title: 'šetrnost stavby',
+                class: 'grades-rectangle--grade--bottomright'
+            }
+        ]
+    })
 }
 </script>
 
@@ -53,7 +59,13 @@ $grades-rectangle-border-radius: 60px;
     grid-template-columns: 1fr 1fr;
     border: 20px solid rgba(0, 0, 0, 0.05);
     border-radius: calc(#{$grades-rectangle-border-radius} + 20px);
-    box-sizing: border-box;
+    box-sizing: content-box;
+}
+
+.grades-rectangle--grade-container {
+    // Needs to be set so the grid items don't increase their height on hover
+    height: 240px;
+    width: 240px;
 }
 
 .grades-rectangle--grade {
@@ -69,24 +81,28 @@ $grades-rectangle-border-radius: 60px;
         width: 50%;
         text-align: center;
         margin-top: 20px;
+        transition: all 0.3s ease-in-out;
+    }
+    &:hover > span {
+        margin-top: 5px;
+        margin-bottom: 0px;
     }
 
-    // > img {
-    //     transition: transform 0.3s ease-in-out;
-    // }
-    // &:hover > img {
-    //     transform: scale(0.8);
-    // }
+    > img {
+        transition: all 0.3s ease-in-out;
+    }
+    &:hover > img {
+        transform: scale(0.80);
+        margin-top: -10px;
+    }
 
-    // &--more {
-    //     max-height: 0;
-    //     opacity: 0;
-    //     transition: all 0.3s ease-in-out;
-    // }
-    // &:hover &--more {
-    //     max-height: 60px;
-    //     opacity: 1;
-    // }
+    &--more {
+        opacity: 0;
+        transition: all 0.3s ease-in-out;
+    }
+    &:hover &--more {
+        opacity: 1;
+    }
     
     &--topleft {
         background-color: $color-support-a;
